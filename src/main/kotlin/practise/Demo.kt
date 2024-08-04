7c6191a5381fd85a1953ac2fdfc0ff739d77006c
IlyaGlaz <glazterix@gmail.com>
2024-08-04 15:11:14 +0300

    practise 4/8

--- src/main/kotlin/practise/Demo.kt
+++ src/main/kotlin/practise/Demo.kt
@@ -1,6 +1,5 @@
 package practise
 
-import java.util.*
 import kotlin.math.max
 
 fun main(args: Array<String>) {
@@ -16,18 +15,22 @@ fun main(args: Array<String>) {
 
 //    println(mult(2, 3))
 
-    // Create a mutable list to store the input numbers
-    val numberList = mutableListOf<Int>()
+//    println(solution(listOf("Hey", "May", "Тesss", "Hey")))
+//    "AD AS AD AU AS  AU".toList()
+//        .windowed(2).forEach(::println)
 
-    // Use a scanner to read the user input
-    val scanner = Scanner(System.`in`)
+    val chunked = "AD".toList()
+        .windowed(2) { list -> list.joinToString(separator = "").replace(" ", "") }
+        .filter { it.length > 1 }
 
-    // TODO: Extract the integers from the input and add them to the mutable list
-    numberList.addAll(scanner.nextLine().split(" ").map { it.toInt() })
+    val result = chunked.groupingBy { it }.eachCount().toList().sortedWith { (key1, value1), (key2, value2) ->
+        when {
+            value1 != value2 -> value2.compareTo(value1)
+            else -> key2.compareTo(key1)
+        }
+    }
 
-    numberList.sort()
-
-    println(numberList.joinToString(" "))
+    println(result[0].first)
 }
 
 fun generate(functionName: String): (Int) -> Int =
@@ -42,3 +45,15 @@ val anonymous = fun(): Int { return 22 }
 val lambda: (Int, Int) -> Int = { a, b -> max(a, b) }
 
 val mult: (Long, Long) -> Long = { a, b -> (a..b).reduce { f, s -> f * s } }
+
+fun solution(products: List<String>): Int {
+    val symbol = "T"
+
+    for ((index, elem) in products.withIndex()) {
+        if (elem.startsWith(symbol) && index % 2 == 1) {
+            return index
+        }
+    }
+    return 0
+}
+
